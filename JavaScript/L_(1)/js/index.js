90e9aa11d9d0de2eaae4203038981a2320cd5c23
Andrii Zadorozhnii <zador746@gmail.com>
2024-07-02 22:35:53 +0800

    add gulp and continue learning JS

---
+++ JavaScript/L_(1)/js/index.js
@@ -0,0 +1,29 @@
+console.log("Hello")
+console.log("World!")
+console.log("3333")
+console.log(3333)
+console.log('Hello' + " world!") // concatenation
+console.log("Hello" + " " + "world!") // concatenation
+
+console.info('Hello')
+
+// alert('Hello')
+// alert('Hello') 
+// alert('Hello')
+
+document.getElementById('out').innerHTML = "Hello";
+document.getElementById('out').innerHTML = 2019;
+document.getElementById('out').innerHTML = "<b>2020</b>";
+
+document.querySelector('.header').innerHTML = "Hello2";
+
+document.querySelector('#lorem').innerHTML = '777';
+document.getElementById('lorem').innerHTML = '888';
+
+let b;
+let a = document.querySelector('#lorem');
+let c;
+c = document.querySelector('.header');
+
+a.innerHTML = 'What do you want to write?';
+c.innerHTML = "Hello my friend";
