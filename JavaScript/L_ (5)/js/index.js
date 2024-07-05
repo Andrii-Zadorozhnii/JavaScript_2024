ad9cfd7defdb616ae071e534545fba024051022a
Andrii Zadorozhnii <zador746@gmail.com>
2024-07-05 13:44:29 +0700

    some exersice

---
+++ JavaScript/L_ (5)/js/index.js
@@ -0,0 +1,29 @@
+let c = '()()(';
+
+
+// Длина строки c.lenght
+console.log(c.length);
+let count = 0
+
+// Цикл
+for (let i = 0; i < c.length; i++) {
+    // console.log(c[i]);
+    if (c[i] == "(") {
+        count++;
+    }
+    if (c[i] == ')') {
+        count--;
+    }
+    if (count < 0) {
+        console.log(false);
+        break
+    }
+
+}
+console.log(count);
+
+if (count != 0) {
+    console.log(false);
+} else {
+    console.log(true);
+}
